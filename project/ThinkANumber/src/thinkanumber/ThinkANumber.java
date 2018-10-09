eb508f2e278cf075d9f6a49a64e4fa6ee09c1749
Judit Keszey <garantalt.siker024@gmail.com>
2018-10-09 21:12:24 +0200

    Az alap project befejezve, fejlesztések elképzelhetőek :)

--- project/ThinkANumber/src/thinkanumber/ThinkANumber.java
+++ project/ThinkANumber/src/thinkanumber/ThinkANumber.java
@@ -1,39 +1,47 @@
-package thinkanumber;
+﻿package thinkanumber;
 
 import java.util.Random;
 import java.util.Scanner;
 
-public class ThinkANumber {
-
+public class ThinkANumber
+{
     private static int alja, teteje, gondolt, tipp;
     private static Scanner sc;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         alja = 1;
         teteje = 100;
         sc = new Scanner(System.in);
         gondolt = Generate();
 
         System.out.println("Gondoltam egy számot " + alja + " és " + teteje + " között, találd ki!");
-        System.out.println("Az én tippem: ");        
+        System.out.println("Az én tippem: ");
         tipp = sc.nextInt();
-        while(tipp < alja || tipp > teteje){
+        while(tipp < alja || tipp > teteje)
+	{
             System.out.println("A tippelt szám a megadott intervallumon kívül esik. Tippeljen újra!");
             tipp = sc.nextInt();
         }
-        while(tipp != gondolt){
-            if(tipp < gondolt){
-                
-            }else{
-                
+        while(tipp != gondolt)
+        {
+            if(tipp < gondolt)
+            {
+                System.out.println("A gondolt szám nagyobb.");
+            	tipp = sc.nextInt();
             }
-        }        
-        
-        //TODO: Megvizsgálni a tipp értékét, aztán ennek megfelelően válaszoljon a program.
-
+            else
+            {
+                System.out.println("A gondolt szám kisebb.");
+            	tipp = sc.nextInt();
+            }
+        }
+ 	System.out.println("A gondolt szám: " + tipp);
     }
+    
 
-    private static int Generate() {
+    private static int Generate()
+    {
         Random rnd = new Random();
         return rnd.nextInt(teteje) + alja;
     }
